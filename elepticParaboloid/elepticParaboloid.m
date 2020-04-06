0bab6a8aeecddb814484231b4633dcf9c8d52b34
Dranikf <kobfedsur@gmail.com>
2020-04-06 05:51:07 +0300

    visualisation start

--- elepticParaboloid/elepticParaboloid.m
+++ elepticParaboloid/elepticParaboloid.m
@@ -2,11 +2,16 @@
 % by Fedor Kobak github.com/Dranikf
 
 [X , Y] = meshGrid(-2:0.1:2 , -2:0.1:2);
-Z = (X.^2) + (Y.^2);
+Z = (X.^2) - 4 * X* Y + 17*(Y.^2) + 5 * Y;
 
 surf(X, Y, Z);
 alpha(0);
 
 hold on;
 plot3(1 ,1 , 2 , 'r.' , 'MarkerSize' , 40);
+
+
+while(true)
+    
+end
 
---
+++ visualisation of steepest descent method/stDesMetVis.m
@@ -0,0 +1,34 @@
+% visualisation of steepest descent method
+% by Fedor Kobak github.com/Dranikf
+
+% expr - is function as a sympolic expression 
+% gridVector - is a base vector for grid
+% P0 - is a start point
+
+% example in course work
+% stDesMetVis(sym('5*(X^2) - 4 * X* Y + 5*(Y^2) - X - Y') , -2:0.1:2)
+
+function stDesMetVis(expr , gridVector , P0)
+    [X , Y] = meshGrid(gridVector , gridVector);
+    
+    vars = symvar(expr);
+    if(numel(vars) ~= 2)
+        disp('incorrect function');
+        return;
+    end
+    
+    if(numel(P0) ~= 2 || min(P0) < min(gridVector) || max(P0) > max(gridVector))
+        disp('incorrect start point');
+    end
+    
+    Z = subs(expr , {X, Y} , {vars(1) , vars(2)});
+
+    surf(X, Y, Z);
+    alpha(0);
+
+    hold on;
+    plot3(P0(1), P0(2) , subs(expr , {vars(1) , vars(2)} , P0) , 'r.' , 'MarkerSize' , 40);
+    
+    
+
+end
